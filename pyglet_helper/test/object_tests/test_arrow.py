ce8fdfa464e5bb421201846d5c38f371576947b7
Catherine <milankie@gmail.com>
2016-04-02 15:15:53 -0700

    created test functions for arrow and ring objects

---
+++ pyglet_helper/test/object_tests/test_arrow.py
@@ -0,0 +1,78 @@
+def test_arrow_degenerate():
+    from pyglet_helper.objects import Arrow
+    from pyglet_helper.util import Vector
+    blo = Arrow(axis=Vector([0, 0, 0]))
+    assert(blo.degenerate)
+
+
+def test_arrow_head_width():
+    from pyglet_helper.objects import Arrow
+    from pyglet_helper.util import Vector
+    blo = Arrow(head_width=0.75)
+    assert(blo.head_width == 0.75)
+    blo = Arrow(shaft_width=0.75)
+    assert(blo.head_width == 1.50)
+    blo = Arrow(axis=Vector([10.0, 0, 0]))
+    assert(blo.head_width == 2.0)
+
+def test_arrow_head_length():
+    from pyglet_helper.objects import Arrow
+    from pyglet_helper.util import Vector
+    blo = Arrow(head_length=0.75)
+    assert(blo.head_length == 0.75)
+    blo = Arrow(shaft_width=0.75)
+    assert(blo.head_length == 2.25)
+    blo = Arrow(axis=Vector([10.0, 0, 0]))
+    assert(blo.head_length == 3.0)
+
+
+def test_arrow_shaft_width():
+    from pyglet_helper.objects import Arrow
+    from pyglet_helper.util import Vector
+    blo = Arrow(shaft_width=0.75)
+    assert(blo.shaft_width == 0.75)
+    blo = Arrow(axis=Vector([10.0, 0, 0]))
+    assert(blo.shaft_width == 1.0)
+
+
+def test_arrow_fixed_width():
+    from pyglet_helper.objects import Arrow
+    blo = Arrow()
+    blo.fixed_width = True
+    assert(blo.fixed_width)
+
+
+def test_arrow_center():
+    from pyglet_helper.objects import Arrow
+    from pyglet_helper.util import Vector
+    blo = Arrow(pos=Vector([-10, -10, -10]), axis=Vector([10, 10, 10]))
+    assert(blo.center[0] == 0)
+    assert(blo.center[1] == 0)
+    assert(blo.center[2] == 0)
+
+
+def test_arrow_effective_geometry():
+    from pyglet_helper.objects import Arrow
+    blo = Arrow(head_width=1.0, shaft_width=1.0, head_length=1.0)
+    assert(blo.effective_geometry(0.2)[0] == 0.2)
+    assert(blo.effective_geometry(0.2)[1] == 0.2)
+    assert(blo.effective_geometry(0.2)[2] == 0.2)
+    blo = Arrow()
+    assert(blo.effective_geometry(1.0)[0] == 0.2)
+    assert(blo.effective_geometry(1.0)[1] == 0.1)
+    assert(blo.effective_geometry(1.0)[2] == 1.0)
+    blo = Arrow()
+    blo.fixed_width = True
+    assert(blo.effective_geometry(1.0)[0] == 0.2)
+    assert(blo.effective_geometry(1.0)[1] == 0.1)
+    assert(blo.effective_geometry(1.0)[2] == 1.0)
+    blo = Arrow(shaft_width=0.001)
+    blo.fixed_width = False
+    assert(blo.effective_geometry(1.0)[0] == 0.04)
+    assert(blo.effective_geometry(1.0)[1] == 0.02)
+    assert(blo.effective_geometry(1.0)[2] == 1.0)
+    blo = Arrow(head_length=1000.0)
+    blo.fixed_width = False
+    assert(blo.effective_geometry(1.0)[0] == 0.0001)
+    assert(blo.effective_geometry(1.0)[1] == 5e-5)
+    assert(blo.effective_geometry(1.0)[2] == 1.0)
---
+++ pyglet_helper/test/object_tests/test_ring.py
@@ -0,0 +1,30 @@
+def test_ring_thickness():
+    from pyglet_helper.objects import Ring
+    blo = Ring(thickness=100000.0)
+    assert(blo.thickness == 100000.0)
+
+
+def test_ring_material_matrix():
+    from pyglet_helper.objects import Ring
+    blo = Ring(thickness=100000.0, radius=0.01)
+    print blo.material_matrix[0, 3]
+    assert(abs(blo.material_matrix[0, 0] - 4.9999995e-08) < 1e-7)
+    assert(abs(blo.material_matrix[1, 1] - 4.9999995e-08) < 1e-7)
+    assert(abs(blo.material_matrix[2, 2] - 4.9999995e-08) < 1e-7)
+    assert(blo.material_matrix[3, 3] == 1.0)
+    assert(blo.material_matrix[3, 0] == 0.5)
+    assert(blo.material_matrix[3, 1] == 0.5)
+    assert(blo.material_matrix[3, 2] == 0.5)
+
+
+def test_ring_degenerate():
+    from pyglet_helper.objects import Ring
+    blo = Ring(radius=100000.0)
+    assert(not blo.degenerate)
+
+
+def test_clamp():
+    from pyglet_helper.objects.ring import clamp
+    assert(clamp(0.5, 0.75, 1.0) == 0.75)
+    assert(clamp(0.5, 1.75, 1.0) == 1.0)
+    assert(clamp(0.5, 0.25, 1.0) == 0.5)
